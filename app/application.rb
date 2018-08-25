e8c79880b88955b1547239de4f1b2ca971d7a2cb
github username <github email address>
2018-08-25 15:21:47 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    t = Time.now
+    t.hour
+    if  t.hour < 12
+    resp.write "Good Morning!"
+    elsif t.hour > 11
+    resp.write "Good Afternoon!"
+    end
+  resp.finish
+  end
+end
